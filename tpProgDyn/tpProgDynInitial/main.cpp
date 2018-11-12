aef75fb8347ef95271a4e5bf0efab11be44c2050
BatyLeo <leo.baty@eleves.enpc.fr>
2018-11-12 19:22:51 +0100

    fin du tp progDyg

--- tpProgDyn/tpProgDynInitial/main.cpp
+++ tpProgDyn/tpProgDynInitial/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <time.h>
 
 
 int dLevenshteinRecursif(std::string s1, std::string s2)
@@ -21,7 +22,7 @@ int dLevenshteinRecursif(std::string s1, std::string s2)
     return min;
 }
 
-int recursionMemoiseLevenshtein(std::string s1, std::string s2, std::vector<std::vector<int> > tab){
+int recursionMemoiseLevenshtein(std::string s1, std::string s2, std::vector<std::vector<int> >& tab){
     int l1 = s1.length();
     int l2 = s2.length();
 
@@ -58,63 +59,51 @@ int dLevenshteinIteratifMemoise(std::string s1, std::string s2)
     int l2 = s2.length();
 
     std::vector<std::vector<int> > tab;
-    std::vector<std::vector<std::string> > operations;
     // Initialisation
-    tab.resize(l1 + 1); operations.resize(l1 + 1);
-    tab[0].resize(l2 + 1); operations[0].resize(l2 + 1);
+    tab.resize(l1 + 1);
+    tab[0].resize(l2 + 1);
     for (int j = 0; j < l2 + 1; j++)
         tab[0][j] = j;
     for (int ligne = 1; ligne < tab.size(); ++ligne) {
-        tab[ligne].resize(l2 + 1, -1); operations[ligne].resize(l2 + 1);
+        tab[ligne].resize(l2 + 1, -1);
         tab[ligne][0] = ligne;
     }
     // Calcul
     for (int i = 1; i < tab.size(); ++i)
         for (int j = 1; j < tab[i].size(); ++j) {
-            // Developpement du min
-            if (tab[i - 1][j] < tab[i][j - 1]){
-                if (1 + tab[i - 1][j] <= tab[i - 1][j - 1] + (s1[i - 1] != s2[j - 1])){
-                    tab[i][j] = 1 + tab[i - 1][j];
-                    operations[i][j] = "suppression";
-                }
-                else{
-                    tab[i][j] = tab[i - 1][j - 1] + (s1[i - 1] != s2[j - 1]);
-                    operations[i][j] = "substitution";
-                }
+            int min = 1 + tab[i][j - 1];
+            min = std::min(min, 1 + tab[i - 1][j]);
+            min = std::min(min, tab[i - 1][j - 1] + (s1[i] != s2[j]));
+            tab[i][j] = min;
+        }
+
+    // On parcours tab afin de recuperer les modifications a apporter a s1 pour obtenir s2
+    int i = l1; int j = l2;
+    while (tab[i][j] != 0){
+        if (tab[i][j - 1] <= tab[i - 1][j]){
+            if (tab[i][j - 1] < tab[i - 1][j - 1] && tab[i][j] == tab[i][j - 1] + 1){
+                std::cout << " - insertion de " + s2.substr(j, 1) + " en " + std::to_string(i + 1) << std::endl;
+                j--;
             }
             else{
-                if (1 + tab[i][j - 1] <= tab[i - 1][j - 1] + (s1[i - 1] != s2[j - 1])){
-                    tab[i][j] = 1 + tab[i][j - 1];
-                    operations[i][j] = "ajout";
-                }
-                else{
-                    tab[i][j] = tab[i - 1][j - 1] + (s1[i - 1] != s2[j - 1]);
-                    operations[i][j] = "substitution";
-                }
+                if (tab[i][j] == tab[i - 1][j - 1] + 1)
+                    std::cout << " - substitution de " + s1.substr(i, 1) + " par " + s2.substr(j, 1) + " en " + std::to_string(i) << std::endl;
+                j--; i--;
             }
         }
-
-    int distance = tab[l1][l2];
-
-    int ligne = l1; int colonne = l2;
-    for (int k = 0; k < distance; ++k){
-        std::string operation = operations[ligne][colonne];
-        if (operation == "ajout"){
-            std::cout << "ajout" << std::endl;
-            colonne--;
-        }
-        else if (operation == "suppression"){
-            std::cout << "suppression" << std::endl;
-            ligne--;
-        }
-        else if (operation == "substitution") {
-            std::cout << "substitution" << std::endl;
-            ligne--; colonne--;
+        else{
+            if (tab[i - 1][j] < tab[i - 1][j - 1] && tab[i][j] == tab[i - 1][j] + 1){
+                std::cout << " - suppression de " + s1.substr(i, 1) + " en " + std::to_string(i) << std::endl;
+                j--;
+            }
+            else{
+                if (tab[i][j] == tab[i - 1][j - 1] + 1)
+                    std::cout << " - substitution de " + s1.substr(i, 1) + " par " + s2.substr(j, 1) + " en " + std::to_string(i) << std::endl;
+                j--; i--;
+            }
         }
     }
-
-    return distance;
-
+    return tab[l1][l2];
 }
 
 int dDamerauLevenshteinRecursif(std::string s1, std::string s2)
@@ -199,13 +188,55 @@ int dDamerauLevenshteinIteratifMemoise(std::string s1, std::string s2)
     return tab[l1][l2];
 }
 
+int dLevenshteinIteratifMemoiseLineaire(std::string s1, std::string s2)
+{
+    int l1 = s1.length();
+    int l2 = s2.length();
+
+    std::vector<std::vector<int> > tab;
+    // Initialisation
+    tab.resize(2);
+    tab[0].resize(l2 + 1);
+    tab[1].resize(l2 + 1, -1);
+    for (int j = 0; j < l2 + 1; j++) {
+        tab[0][j] = j;
+    }
+    tab[1][0] = 1;
+    // Calcul
+    for (int i = 1; i < l1 - 1; ++i) {
+        for (int j = 1; j < tab[0].size(); ++j) {
+            int min = 1 + tab[1][j - 1];
+            min = std::min(min, 1 + tab[0][j]);
+            min = std::min(min, tab[0][j - 1] + (s1[i] != s2[j]));
+            tab[1][j] = min;
+        }
+        for (int j = 0; j < l2; j++) {
+            tab[0][j] = tab[1][j];
+            tab[1][j] = -1;
+        }
+        tab[1][0] = i + 1;
+    }
+
+    return tab[1][l2];
+}
+
 int main() {
-    std::string a = "eoc"; std::string b = "eco";
-    std::cout << "Levenshtein recursif : " << dLevenshteinRecursif(a, b) << std::endl;
-    std::cout << "Levenshtein recursif memoise : " << dLevenshteinRecursifMemoise(a, b) << std::endl;
-    std::cout << "Levenshtein iteratif memoise : \n" << dLevenshteinIteratifMemoise(a, b) << std::endl;
+    std::string a = "ecoles"; std::string b = "eclose";
+    clock_t t1 = clock();
+    std::cout << "Levenshtein recursif : " << dLevenshteinRecursif(a, b);
+    clock_t t2 = clock();
+    std::cout << ", " << (float)(t2-t1)/CLOCKS_PER_SEC << "s" << std::endl;
+    std::cout << "Levenshtein recursif memoise : " << dLevenshteinRecursifMemoise(a, b);
+    clock_t t3 = clock();
+    std::cout << ", " << (float)(t3-t2)/CLOCKS_PER_SEC << "s" << std::endl;
+    std::cout << "Levenshtein iteratif memoise :\n" << dLevenshteinIteratifMemoise(a, b);
+    clock_t t4 = clock();
+    std::cout << ", " << (float)(t4-t3)/CLOCKS_PER_SEC << "s" << std::endl;
+    std::cout << "Levenshtein iteratif memoise lineaire : " << dLevenshteinIteratifMemoiseLineaire(a, b);
+    clock_t t5 = clock();
+    std::cout << ", " << (float)(t5-t4)/CLOCKS_PER_SEC << "s" << std::endl;
     std::cout << "Damerau-Levenshtein recursif : " << dDamerauLevenshteinRecursif(a, b) << std::endl;
     std::cout << "Damerau-Levenshtein recursif memoise : " << dDamerauLevenshteinRecursifMemoise(a, b) << std::endl;
-    std::cout << "Damerau-Levenshtein iteratif memoise : " << dDamerauLevenshteinIteratifMemoise(a, b);
+    std::cout << "Damerau-Levenshtein iteratif memoise : " << dDamerauLevenshteinIteratifMemoise(a, b) << std::endl;
     return 0;
 }
